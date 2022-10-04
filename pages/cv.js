d69163a66ac9b965e053510b3e4a90eb6d0797af
rakotoew <55093676+rakotoew@users.noreply.github.com>
2022-10-04 11:38:50 +0200

    small fix

--- pages/cv.js
+++ pages/cv.js
@@ -1,15 +1,15 @@
 import styles from "../styles/utils.module.css";
 import Layout from "../components/layout";
 import {useTranslations} from "use-intl";
-import {Document, Page} from "react-pdf";
 
 export default function cv() {
-
     const t = useTranslations("generic");
     return(
         <Layout className={styles.main}>
             <div>
-                <iframe src='/cv1.pdf' width="1200"
+                <iframe title={t("cv.title")}
+                        src='/cv1.pdf'
+                        width="1200"
                         height="1800"/>
             </div>
         </Layout>
